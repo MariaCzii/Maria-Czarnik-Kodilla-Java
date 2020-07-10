31b51db4ca2bae258b3f5ca55f98756fbf358e77
MariaCzii <maria.czarnik9@gmail.com>
2020-07-10 13:05:11 +0400

    First Commit

--- src/test/java/com/kodilla/KodillaCourseApplicationTests.java
+++ src/test/java/com/kodilla/KodillaCourseApplicationTests.java
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KodillaCourseApplicationTests {
+    public static void main(String[] args) {
 
-    @Test
-    void contextLoads() {
     }
 
 }
