f8e02c732ef8a220fc80e72583fcbff945110615
MariaCzii <maria.czarnik9@gmail.com>
2020-07-10 22:41:01 +0400

    First Commit

---
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+        public int addAToB ( int a, int b){
+            return a + b;
+        }
+
+        public int subtractBFromA ( int a, int b){
+            return a - b;
+        }
+    }
+
